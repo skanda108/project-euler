d8c9d3f834dcbb760b759c92b280759053855a8f
skanda108 <sbthai94@gmail.com>
2023-11-01 12:57:39 -0500

    created sieve algorithm for primes

---
+++ sieve.js
@@ -0,0 +1,18 @@
+// Creating the sieve of eratosthenes since finding prime numbers is important
+
+const eratos = (num) => {
+  const sieve = [];
+  const primes = [];
+
+  for (let i = 2; i <= num; i++) {
+    if (!sieve.includes(i)) {
+      primes.push(i);
+      for (let j = i*2; j <= num; j+=i) {
+        sieve.push(j);
+      }
+    }
+  }
+  return primes;
+};
+
+console.log(eratos(121));
